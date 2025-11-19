5a08d7226e98b151bae210ddffba5ff3be73cb10
MiguelSantosIzaltino <contato.izaltinoo@gmail.com>
2025-11-19 16:13:53 -0300

    colocando delete,atualizar e listar todos no service e no controller.

--- src/main/java/br/com/senai/backend/sistema_mercado/services/FuncionarioService.java
+++ src/main/java/br/com/senai/backend/sistema_mercado/services/FuncionarioService.java
@@ -1,9 +1,12 @@
 package br.com.senai.backend.sistema_mercado.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.senai.backend.sistema_mercado.models.Funcionario;
+
 import br.com.senai.backend.sistema_mercado.repositories.FuncionarioRepository;
 
 @Service
@@ -18,5 +21,27 @@ public class FuncionarioService {
 
     public Funcionario recuperarPorId(Integer id) {
         return funcionarioRepository.findById(id).get();
+    }
+
+    public List<Funcionario> listarTodos() {
+        return funcionarioRepository.findAll();
+    }
+
+    public Funcionario atualizar(Integer id, Funcionario funcionario) { //informando os dados para att
+        Funcionario merc = funcionarioRepository.findById(id).get(); //indo no banco de dados procurar ids
+        if(merc != null) { //identificou que esse objeto existe
+        funcionario.setId(merc.getId()); //setar o id com um id existente
+        return funcionarioRepository.save(funcionario); //retorna 
+        }
+        return null; //se tiver algum id retornara nulo
+    }
+
+    public boolean removerPorId(Integer id) {
+        Funcionario merc = funcionarioRepository.findById(id).get();
+        if(merc != null) {
+            funcionarioRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/br/com/senai/backend/sistema_mercado/services/MercadoriaService.java
+++ src/main/java/br/com/senai/backend/sistema_mercado/services/MercadoriaService.java
@@ -1,5 +1,7 @@
 package br.com.senai.backend.sistema_mercado.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +20,27 @@ public class MercadoriaService {
 
     public Mercadoria recuperarPorId(Integer id) {
         return mercadoriaRepository.findById(id).get();
+    }
+
+    public List<Mercadoria> listarTodos() {
+        return mercadoriaRepository.findAll();
+    }
+
+    public Mercadoria atualizar(Integer id, Mercadoria mercadoria) { //informando os dados para att
+        Mercadoria merc = mercadoriaRepository.findById(id).get(); //indo no banco de dados procurar ids
+        if(merc != null) { //identificou que esse objeto existe
+        mercadoria.setId(merc.getId()); //setar o id com um id existente
+        return mercadoriaRepository.save(mercadoria); //retorna 
+        }
+        return null; //se tiver algum id retornara nulo
+    }
+
+    public boolean removerPorId(Integer id) {
+        Mercadoria merc = mercadoriaRepository.findById(id).get();
+        if(merc != null) {
+            mercadoriaRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
